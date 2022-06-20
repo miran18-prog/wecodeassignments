24bcc28a7a07cbce18221e37282759ae52bc1f4b
miran amanj <miran.am2001@gmail.com>
2022-06-20 17:02:44 +0300

    Assignment/ Changing image after clicking

--- lib/Page1.dart
+++ lib/Page1.dart
@@ -2,33 +2,59 @@
 
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String ImageName = "1";
+  Widget changeImage(String image) {
+    return InkWell(
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      hoverColor: Colors.transparent,
+      child: Image.asset(
+        'Assets/$image.png',
+        height: 110,
+        width: 110,
+      ),
+      onTap: () {
+        setState(() {
+          ImageName = image;
+        });
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(90),
-              child: Image.network(
-                'https://th.bing.com/th/id/R.13c09f865f2de250568fa079980d4bb5?rik=JBy2HHCKLKuHHQ&riu=http%3a%2f%2fwww.wallpaperbetter.com%2fwallpaper%2f79%2f638%2f538%2fkurdistan-flag-1080P-wallpaper.jpg&ehk=2SOhKrjKQmTtD0gU5fGSbnU9NXjmA3TXzY1KMSXQlV0%3d&risl=&pid=ImgRaw&r=0',
-              ),
+        home: Scaffold(
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              "Slider",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
             ),
-          ),
-          SizedBox(
-            height: 75,
-          ),
-          Text(
-            "Hello Kurdistan :)",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-          ),
-        ],
-      )),
-    );
+            Image.asset(
+              'Assets/$ImageName.png',
+              height: 150,
+              width: 150,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                changeImage("1"),
+                changeImage("2"),
+                changeImage("3"),
+              ],
+            )
+          ]),
+    ));
   }
 }
